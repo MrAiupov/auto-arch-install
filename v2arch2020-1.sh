a901347a34b7e8d38ae97b7aff71824d3fbdafea
Airat Aiupov <47436050+MrAiupov@users.noreply.github.com>
2020-02-18 03:09:31 +0500

    Create v2arch2020-1.sh

---
+++ v2arch2020-1.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# Arch Linux Fast Install - Быстрая установка Arch Linux
+# Цель скрипта - быстрое развертывание системы с вашими персональными настройками
+
+loadkeys ru
+setfont cyr-sun16
+
+echo '2.3 Синхронизация системных часов'
+timedatectl set-ntp true
+
+echo '2.4 создание разделов'
+(
+  echo o;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +500M;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +20G;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +2048M;
+
+  echo n;
+  echo p;
+  echo;
+  echo;
+  echo a;
+  echo 1;
+
+  echo w;
+) | fdisk /dev/sda
+
+echo 'Ваша разметка диска'
+fdisk -l
+
+echo '2.4.2 Форматирование дисков'
+mkfs.ext2  /dev/sda1 -L boot
+mkfs.ext4  /dev/sda2 -L root
+mkswap /dev/sda3 -L swap
+mkfs.ext4  /dev/sda4 -L home
+
+echo '2.4.3 Монтирование дисков'
+mount /dev/sda2 /mnt
+mkdir /mnt/{boot,home}
+mount /dev/sda1 /mnt/boot
+swapon /dev/sda3
+mount /dev/sda4 /mnt/home
+
+echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
+echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+
+echo '3.2 Установка основных пакетов'
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
+
+echo '3.3 Настройка системы'
+genfstab -pU /mnt >> /mnt/etc/fstab
+
+read -p "Введите имя компьютера: " hostname
+read -p "Введите имя пользователя: " username
+
+echo 'Прописываем имя компьютера'
+echo $hostname > /etc/hostname
+ln -svf /usr/share/zoneinfo/Asia/Yekaterinburg /etc/localtime
+
+echo '3.4 Добавляем русскую локаль системы'
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
+localectl set-x11-keymap us,ru pc104 "" grp:alt_shift_toggle
+
+echo 'Обновим текущую локаль системы'
+locale-gen
+
+echo 'Указываем язык системы'
+echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
+
+echo 'Вписываем KEYMAP=ru FONT=cyr-sun16'
+echo 'KEYMAP=ru' >> /etc/vconsole.conf
+echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
+
+echo 'Создадим загрузочный RAM диск'
+mkinitcpio -p linux
+
+echo '3.5 Устанавливаем загрузчик'
+pacman -Syy
+pacman -S grub --noconfirm 
+grub-install /dev/sda
+
+echo 'Обновляем grub.cfg'
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo 'Ставим программу для Wi-fi'
+pacman -S dialog wpa_supplicant --noconfirm 
+
+echo 'Добавляем пользователя'
+useradd -m -g users -G wheel -s /bin/bash $username
+
+echo 'Создаем root пароль'
+passwd
+
+echo 'Устанавливаем пароль пользователя'
+passwd $username
+
+echo 'Устанавливаем SUDO'
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+
+echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+pacman -Syy
+
+echo 'Устанавливаем xorg и его драйвера'
+pacman -S xorg-server xorg-drivers xorg-xinit --noconfirm 
+
+echo 'Устанавливаем часто используемый шрифт, плюс поддержку иероглифоф'
+pacman -S ttf-liberation ttf-dejavu wqy-zenhei --noconfirm 
+
+echo 'Устанавливаем поддержку сети'
+pacman -S networkmanager network-manager-applet ppp --noconfirm
+
+echo 'Устанавливаем поддержку ntfs разделов'
+pacman -S ntfs-3g ufw --noconfirm
+
+echo 'Установка аудиодрайверов'
+pacman -S alsa-lib alsa-utils lib32-alsa-plugins lib32-curl --noconfirm
+
+echo 'Включаем автоподключение интернета'
+systemctl enable NetworkManager
+
+echo 'Установка завершена! Перезагрузите систему.'
+exit
+```
