3b2ba8c5093d11c2cccde8c090c65339a78ef51b
Airat Aiupov <47436050+MrAiupov@users.noreply.github.com>
2020-09-07 00:28:40 +0500

    Create ArchLinuxAiratClear1.sh

---
+++ ArchLinuxAiratClear1.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Arch Linux Fast Install - Быстрая установка Arch Linux https://github.com/ordanax/arch2018
+# Цель скрипта - быстрое развертывание системы с вашими персональными настройками (конфиг, темы, программы и т.д.).
+
+loadkeys ru
+setfont cyr-sun16
+
+echo '2.3 Синхронизация системных часов'
+timedatectl set-ntp true
+
+echo '2.4.2 Форматирование и монтирование дисков'
+mkfs.ext4 /dev/sda4
+mount /dev/sda4 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+echo 'Дополнительные разделы'
+mkdir /mnt/windows
+mkdir /mnt/hard1
+mount /dev/sda3 /mnt/windows
+mount /dev/sdb1 /mnt/hard1
+
+echo '3.1 Выбор зеркал для загрузки. Ставим зеркала для России'
+echo "##" > /etc/pacman.d/mirrorlist
+echo "##Arch Linux repository mirrorlist" >> /etc/pacman.d/mirrorlist
+echo "##Generated on 2020-02-19" >> /etc/pacman.d/mirrorlist
+echo "##" >> /etc/pacman.d/mirrorlist
+echo "##Russia" >> /etc/pacman.d/mirrorlist
+echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+echo "Server = https://mirror.yandex.ru/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+echo "Server = http://mirror.truenetwork.ru/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+echo "Server = http://mirror.rol.ru/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+echo "Server = https://mirror.rol.ru/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+echo "Server = http://archlinux.zepto.cloud/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+
+echo '3.2 Установка основных пакетов'
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
+
+echo '3.3 Настройка системы'
+genfstab -pU /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/JUG7c)"
